5515c88e3483fe28f578e34fb67caf3011465425
Nicat <cnicat2323@gmail.com>
2024-04-24 11:35:39 +0400

    FileStream-File-IO

---
+++ ConsoleApp21/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+        [Serializable]
+        public class Person : ISerializable
+        {
+            private static int _id;
+            public int Id { get; private set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+
+            public Person()
+            {
+                _id++;
+                Id = _id;
+            }
+            public void GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                info.AddValue("Id", Id, typeof(Person));
+                info.AddValue("Name", Name, typeof(Person));
+                info.AddValue("Surnam", Surname, typeof(Person));
+            }
+
+            public override string? ToString()
+            {
+                return $"Id: {Id} Name: {Name} Surname: {Surname}";
+            }
+        }
+    }
+
---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp21;
+using System;
+using System.Text.Json;
+
+namespace JsonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Nicat";
+            person.Surname = "Cabbarov";
+
+            // Serialize
+            PersonToJSON(person);
+
+            // Deserialize
+            Person deserializedPerson = JSONToPerson();
+            Console.WriteLine(deserializedPerson);
+        }
+
+        public static void PersonToJSON(Person person)
+        {
+            string path = "D:\\Users\\User\\Desktop\\data.json";
+
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                JsonSerializer.Serialize(stream, person);
+            }
+        }
+
+        public static Person JSONToPerson()
+        {
+            string path = "D:\\Users\\User\\Desktop\\data.json";
+
+            using (Stream stream = new FileStream(path, FileMode.Open))
+            {
+
+                return JsonSerializer.Deserialize<Person>(stream);
+            }
+        }
+
+    }
+}
